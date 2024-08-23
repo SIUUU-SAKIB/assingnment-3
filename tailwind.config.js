b218acaa1d012e5dfd120a9c4278dcb1ca751034
SIUUU-SAKIB <sakibmia7254@gmail.com>
2024-08-24 00:31:43 +0600

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {
+      height:{
+        maxHeight:`600px`
+      },
       screens:{
         sm:`450px`
       },
@@ -11,7 +14,7 @@ module.exports = {
         navTxtCol: `#737373`,
         subTxtCol:`#494949`,
         btnBg : `#ABEF5F`,
-        bgColor : `#e2e8f0`,
+        bgColor : `#e2e8f061`,
         card1 : `#E7C1D3`,
         card2 : `#EFDA6E`,
         card3 : `#A4DAC3`,
